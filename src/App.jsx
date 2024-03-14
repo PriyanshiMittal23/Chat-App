d5d85657b43fc7b28936a9f7e7d4034f2a21e9bb
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-03-14 12:18:16 +0530

    Chat_App Intialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {auth, db, storage} from './firebase'
+
+const App = () => {
+  return (
+    <div>
+      {/* {JSON.stringify(storage)}; */}
+    </div>   
+  )
+}
+
+export default App
